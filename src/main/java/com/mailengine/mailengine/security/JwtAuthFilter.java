d426011d182c64092c6c4a2bc9970b4f6f974077
op-techy <opeoluwa.babalola@stu.cu.edu.ng>
2026-04-09 10:47:00 +0100

    feat: finalize PRD-compliant recipient logic with S3 archival and suppression check

--- src/main/java/com/mailengine/mailengine/security/JwtAuthFilter.java
+++ src/main/java/com/mailengine/mailengine/security/JwtAuthFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
